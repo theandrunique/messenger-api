15bfd599e75b82e70096fb005a375f527f39858d
theandrunique <theandrunique@gmail.com>
2024-09-07 18:07:01 +0300

    Register new services and update authentication

--- MessengerAPI.Infrastructure/DependencyInjection.cs
+++ MessengerAPI.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,13 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using MessengerAPI.Application.Common.Interfaces;
 using MessengerAPI.Application.Common.Interfaces.Auth;
 using MessengerAPI.Application.Common.Interfaces.Persistance;
 using MessengerAPI.Infrastructure.Auth;
 using MessengerAPI.Infrastructure.Common;
+using MessengerAPI.Infrastructure.Common.FileStorage;
 using MessengerAPI.Infrastructure.Common.Persistance;
+using MessengerAPI.Infrastructure.Persistance;
 using MessengerAPI.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +30,10 @@ public static class DependencyInjection
         services.AddSingleton<IHashHelper, BCryptHelper>();
         services.AddScoped<IUserAgentParser, UserAgentParser>();
         services.AddScoped<IJweHelper, JweHelper>();
+        services.AddSingleton<ITokenCacheService, InMemoryTokenCacheService>();
+
+        services.Configure<FileStorageSettings>(config.GetSection(nameof(FileStorageSettings)));
+        services.AddScoped<IFileStorage, FileStorage>();
 
         return services;
     }
@@ -36,6 +43,8 @@ public static class DependencyInjection
         services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=app.db"));
 
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IFileRepository, fileRepository>();
+        services.AddScoped<IChatRepository, ChatRepository>();
 
         return services;
     }
@@ -52,18 +61,51 @@ public static class DependencyInjection
         services.AddSingleton<IJwtTokenGenerator, JwtTokenHelper>();
 
         services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            .AddJwtBearer(options =>
             {
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                ValidIssuer = jwtSettings.Issuer,
-                ValidAudience = jwtSettings.Audience,
-                IssuerSigningKey = new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(jwtSettings.Secret))
-            });
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                };
+                options.MapInboundClaims = false;
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = async context =>
+                    {
+                        Dictionary<string, string>? claims = context.Principal?.Claims.ToDictionary(c => c.Type, c => c.Value);
+                        if (claims == null)
+                        {
+                            context.Fail("Invalid token.");
+                            return;
+                        }
 
+                        if (!claims.ContainsKey(JwtRegisteredClaimNames.Sub))
+                        {
+                            context.Fail("Invalid token.");
+                            return;
+                        }
+                        if (!claims.ContainsKey(JwtRegisteredClaimNames.Jti))
+                        {
+                            context.Fail("Invalid token.");
+                            return;
+                        }
+
+                        var tokenId = claims[JwtRegisteredClaimNames.Jti];
+
+                        var cacheService = context.HttpContext.RequestServices.GetRequiredService<ITokenCacheService>();
+                        if (!await cacheService.IsTokenValidAsync(tokenId))
+                        {
+                            context.Fail("Token has been revoked.");
+                        }
+                    }
+                };
+            });
         return services;
     }
 }
