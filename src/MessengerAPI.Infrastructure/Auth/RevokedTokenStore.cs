7f66476a155e34d289e5164bfdd5e7aa9f368996
theandrunique <theandrunique@gmail.com>
2024-10-02 09:25:08 +0300

    Fix the issue with exparation time

--- src/MessengerAPI.Infrastructure/Auth/RevokedTokenStore.cs
+++ src/MessengerAPI.Infrastructure/Auth/RevokedTokenStore.cs
@@ -6,22 +6,25 @@ namespace MessengerAPI.Infrastructure.Auth;
 public class RevokedTokenStore : IRevokedTokenStore
 {
     private readonly IDatabase _redis;
-    private readonly string key = "revokedTokens";
+    private readonly string key = "revokedToken";
 
     public RevokedTokenStore(IConnectionMultiplexer redis)
     {
         _redis = redis.GetDatabase();
     }
 
+    private string GetKey(Guid tokenId)
+    {
+        return $"{key}:{tokenId}";
+    }
+
     public async Task<bool> IsTokenValidAsync(Guid tokenId)
     {
-        return !await _redis.SetContainsAsync(key, tokenId.ToString());
+        return !await _redis.KeyExistsAsync(GetKey(tokenId));
     }
 
     public async Task RevokeTokenAsync(Guid tokenId, int expires)
     {
-        await _redis.SetAddAsync(key, tokenId.ToString());
-
-        await _redis.KeyExpireAsync(key, TimeSpan.FromSeconds(expires));
+        await _redis.StringSetAsync(GetKey(tokenId), "true", expiry: TimeSpan.FromSeconds(expires));
     }
 }
