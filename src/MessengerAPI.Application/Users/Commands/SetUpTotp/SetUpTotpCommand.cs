2cea8136a09b60f541a6fc1e044b1a5cf9e5b6be
theandrunique <theandrunique@gmail.com>
2025-02-14 17:56:45 +0300

    Remove 'SetUpTotpCommand' for now

--- src/MessengerAPI.Application/Users/Commands/SetUpTotp/SetUpTotpCommand.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-using MessengerAPI.Errors;
-
-namespace MessengerAPI.Application.Users.Commands.SetUpTotp;
-
-public record SetUpTotpCommand() : IRequest<ErrorOr<SetUpTotpCommandResponse>>;
--- src/MessengerAPI.Application/Users/Commands/SetUpTotp/SetUpTotpCommandResponse.cs
+++
@@ -1,3 +0,0 @@
-namespace MessengerAPI.Application.Users.Commands.SetUpTotp;
-
-public record SetUpTotpCommandResponse(string OtpAuthUrl);
