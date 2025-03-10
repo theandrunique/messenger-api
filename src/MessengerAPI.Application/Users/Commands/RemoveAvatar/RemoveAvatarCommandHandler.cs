47b493d8e0ca1a9692e9608711eea6b6bcd97992
theandrunique <theandrunique@gmail.com>
2025-03-10 12:14:39 +0300

    Implement removing user avatar command

--- src/MessengerAPI.Application/Users/Commands/RemoveAvatar/RemoveAvatarCommandHandler.cs
+++ src/MessengerAPI.Application/Users/Commands/RemoveAvatar/RemoveAvatarCommandHandler.cs
@@ -1,11 +1,30 @@
 using MediatR;
+using MessengerAPI.Application.Common.Interfaces;
+using MessengerAPI.Data.Users;
 
 namespace MessengerAPI.Application.Users.Commands.RemoveAvatar;
 
 public class RemoveAvatarCommandHandler : IRequestHandler<RemoveAvatarCommand, Unit>
 {
-    public Task<Unit> Handle(RemoveAvatarCommand request, CancellationToken cancellationToken)
+    private readonly IUserRepository _userRepository;
+    private readonly IClientInfoProvider _clientInfo;
+
+    public RemoveAvatarCommandHandler(IUserRepository userRepository, IClientInfoProvider clientInfo)
     {
-        throw new NotImplementedException();
+        _userRepository = userRepository;
+        _clientInfo = clientInfo;
+    }
+
+    public async Task<Unit> Handle(RemoveAvatarCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdOrNullAsync(_clientInfo.UserId);
+        if (user is null)
+        {
+            throw new Exception("User was expected to be found here.");
+        }
+
+        user.RemoveAvatar();
+        await _userRepository.UpdateAvatarAsync(user);
+        return Unit.Value;
     }
 }
